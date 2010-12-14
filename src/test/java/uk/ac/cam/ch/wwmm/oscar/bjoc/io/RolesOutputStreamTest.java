a5e3fe4827e9c5f158265eda035c7db72ce88766
Egon Willighagen <egon.willighagen@gmail.com>
2010-12-14 14:52:06 +0100

    Added wellformedness tests for written results

---
+++ src/test/java/uk/ac/cam/ch/wwmm/oscar/bjoc/io/RolesOutputStreamTest.java
@@ -0,0 +1,25 @@
+package uk.ac.cam.ch.wwmm.oscar.bjoc.io;
+
+import java.io.ByteArrayOutputStream;
+
+import junit.framework.Assert;
+import nu.xom.Builder;
+import nu.xom.Document;
+
+import org.junit.Test;
+
+public class RolesOutputStreamTest {
+
+	@Test
+	public void testWellformedness() throws Exception {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		RolesOutputStream chemOut = new RolesOutputStream(out);
+		chemOut.close();
+		String results = new String(out.toByteArray());
+		System.out.println(results);
+		Builder parser = new Builder();
+		Document doc = parser.build(results, "http://www.example.org/");
+		Assert.assertNotNull(doc);
+	}
+
+}
