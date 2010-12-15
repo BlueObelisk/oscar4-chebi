0e3ed6f5a34770edda357a75e7d94c84649e7085
Egon Willighagen <egon.willighagen@gmail.com>
2010-12-15 17:21:36 +0100

    More elaborate unit testing: output of RecoveredChemistry objects

--- src/test/java/uk/ac/cam/ch/wwmm/oscar/bjoc/io/RecoveredChemistryOutputStreamTest.java
+++ src/test/java/uk/ac/cam/ch/wwmm/oscar/bjoc/io/RecoveredChemistryOutputStreamTest.java
@@ -8,13 +8,59 @@ import nu.xom.Document;
 
 import org.junit.Test;
 
+import uk.ac.cam.ch.wwmm.oscar.bjoc.RecoveredChemistry;
+
 public class RecoveredChemistryOutputStreamTest {
 
 	@Test
-	public void testWellformedness() throws Exception {
+	public void testWellformedness_Wrapper() throws Exception {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		RecoveredChemistryOutputStream chemOut =
-			new RecoveredChemistryOutputStream(out);
+		RecoveredChemistryOutputStream chemOut = new RecoveredChemistryOutputStream(out);
+		chemOut.close();
+		String results = new String(out.toByteArray());
+		Builder parser = new Builder();
+		Document doc = parser.build(results, "http://www.example.org/");
+		Assert.assertNotNull(doc);
+	}
+
+	@Test
+	public void testValidness_Wrapper() throws Exception {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		RecoveredChemistryOutputStream chemOut = new RecoveredChemistryOutputStream(out);
+		chemOut.close();
+		String results = new String(out.toByteArray());
+		Builder parser = new Builder();
+		Document doc = parser.build(results, "http://www.example.org/");
+		Assert.assertNotNull(doc);
+	}
+
+	@Test
+	public void testWellformedness_Crash() throws Exception {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		RecoveredChemistryOutputStream chemOut = new RecoveredChemistryOutputStream(out);
+		RecoveredChemistry chemistry = new RecoveredChemistry("1");
+		chemistry.addCrash(
+			"This sentence caused a crash.",
+			new Exception("Something went wrong.")
+		);
+		chemOut.write(chemistry);
+		chemOut.close();
+		String results = new String(out.toByteArray());
+		Builder parser = new Builder();
+		Document doc = parser.build(results, "http://www.example.org/");
+		Assert.assertNotNull(doc);
+	}
+
+	@Test
+	public void testValidness_Crash() throws Exception {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		RecoveredChemistryOutputStream chemOut = new RecoveredChemistryOutputStream(out);
+		RecoveredChemistry chemistry = new RecoveredChemistry("1");
+		chemistry.addCrash(
+			"This sentence caused a crash.",
+			new Exception("Something went wrong.")
+		);
+		chemOut.write(chemistry);
 		chemOut.close();
 		String results = new String(out.toByteArray());
 		Builder parser = new Builder();
