f22fa2a1d7e8179062abc1c23c6aa491cc0554ba
Egon Willighagen <egonw@users.noreply.github.com>
2010-12-15 17:21:36 +0100

    More elaborate unit testing: output of RecoveredChemistry objects

--- src/test/java/uk/ac/cam/ch/wwmm/oscar/bjoc/io/RolesOutputStreamTest.java
+++ src/test/java/uk/ac/cam/ch/wwmm/oscar/bjoc/io/RolesOutputStreamTest.java
@@ -8,12 +8,59 @@ import nu.xom.Document;
 
 import org.junit.Test;
 
+import uk.ac.cam.ch.wwmm.oscar.bjoc.RecoveredChemistry;
+
 public class RolesOutputStreamTest {
 
 	@Test
-	public void testWellformedness() throws Exception {
+	public void testWellformedness_Wrapper() throws Exception {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		RolesOutputStream chemOut = new RolesOutputStream(out);
+		chemOut.close();
+		String results = new String(out.toByteArray());
+		Builder parser = new Builder();
+		Document doc = parser.build(results, "http://www.example.org/");
+		Assert.assertNotNull(doc);
+	}
+
+	@Test
+	public void testValidness_Wrapper() throws Exception {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		RolesOutputStream chemOut = new RolesOutputStream(out);
+		chemOut.close();
+		String results = new String(out.toByteArray());
+		Builder parser = new Builder();
+		Document doc = parser.build(results, "http://www.example.org/");
+		Assert.assertNotNull(doc);
+	}
+
+	@Test
+	public void testWellformedness_Crash() throws Exception {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		RolesOutputStream chemOut = new RolesOutputStream(out);
+		RecoveredChemistry chemistry = new RecoveredChemistry("1");
+		chemistry.addCrash(
+			"This sentence caused a crash.",
+			new Exception("Something went wrong.")
+		);
+		chemOut.write(chemistry);
+		chemOut.close();
+		String results = new String(out.toByteArray());
+		Builder parser = new Builder();
+		Document doc = parser.build(results, "http://www.example.org/");
+		Assert.assertNotNull(doc);
+	}
+
+	@Test
+	public void testValidness_Crash() throws Exception {
+		ByteArrayOutputStream out = new ByteArrayOutputStream();
+		RolesOutputStream chemOut = new RolesOutputStream(out);
+		RecoveredChemistry chemistry = new RecoveredChemistry("1");
+		chemistry.addCrash(
+			"This sentence caused a crash.",
+			new Exception("Something went wrong.")
+		);
+		chemOut.write(chemistry);
 		chemOut.close();
 		String results = new String(out.toByteArray());
 		Builder parser = new Builder();
