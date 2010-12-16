461d8584bd7e1059361ff6b3fa1ab5eb44783b55
Egon Willighagen <egon.willighagen@gmail.com>
2010-12-16 12:47:59 +0100

    More detailed testing, and removed the validity testing, which fails because of Xerces not playing nicely

--- src/test/java/uk/ac/cam/ch/wwmm/oscar/bjoc/io/RecoveredChemistryOutputStreamTest.java
+++ src/test/java/uk/ac/cam/ch/wwmm/oscar/bjoc/io/RecoveredChemistryOutputStreamTest.java
@@ -8,23 +8,13 @@ import nu.xom.Document;
 
 import org.junit.Test;
 
+import uk.ac.cam.ch.wwmm.oscar.Oscar;
 import uk.ac.cam.ch.wwmm.oscar.bjoc.RecoveredChemistry;
 
 public class RecoveredChemistryOutputStreamTest {
 
 	@Test
 	public void testWellformedness_Wrapper() throws Exception {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		RecoveredChemistryOutputStream chemOut = new RecoveredChemistryOutputStream(out);
-		chemOut.close();
-		String results = new String(out.toByteArray());
-		Builder parser = new Builder();
-		Document doc = parser.build(results, "http://www.example.org/");
-		Assert.assertNotNull(doc);
-	}
-
-	@Test
-	public void testValidness_Wrapper() throws Exception {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		RecoveredChemistryOutputStream chemOut = new RecoveredChemistryOutputStream(out);
 		chemOut.close();
@@ -52,17 +42,18 @@ public class RecoveredChemistryOutputStreamTest {
 	}
 
 	@Test
-	public void testValidness_Crash() throws Exception {
+	public void testWellformedness_NamedEntities() throws Exception {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		RecoveredChemistryOutputStream chemOut = new RecoveredChemistryOutputStream(out);
 		RecoveredChemistry chemistry = new RecoveredChemistry("1");
-		chemistry.addCrash(
-			"This sentence caused a crash.",
-			new Exception("Something went wrong.")
-		);
+		Oscar oscar = new Oscar();
+		chemistry.addNamedEntities(oscar.getNamedEntities(
+			"Benzene was mixed with toluene."
+		));
 		chemOut.write(chemistry);
 		chemOut.close();
 		String results = new String(out.toByteArray());
+		Assert.assertTrue(results.contains("Benzene"));
 		Builder parser = new Builder();
 		Document doc = parser.build(results, "http://www.example.org/");
 		Assert.assertNotNull(doc);
--- src/test/java/uk/ac/cam/ch/wwmm/oscar/bjoc/io/RolesOutputStreamTest.java
+++ src/test/java/uk/ac/cam/ch/wwmm/oscar/bjoc/io/RolesOutputStreamTest.java
@@ -1,6 +1,8 @@
 package uk.ac.cam.ch.wwmm.oscar.bjoc.io;
 
 import java.io.ByteArrayOutputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import junit.framework.Assert;
 import nu.xom.Builder;
@@ -8,23 +10,14 @@ import nu.xom.Document;
 
 import org.junit.Test;
 
+import uk.ac.cam.ch.wwmm.chemicaltagger.roles.NamedEntityWithRoles;
+import uk.ac.cam.ch.wwmm.chemicaltagger.roles.Role;
 import uk.ac.cam.ch.wwmm.oscar.bjoc.RecoveredChemistry;
 
 public class RolesOutputStreamTest {
 
 	@Test
 	public void testWellformedness_Wrapper() throws Exception {
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		RolesOutputStream chemOut = new RolesOutputStream(out);
-		chemOut.close();
-		String results = new String(out.toByteArray());
-		Builder parser = new Builder();
-		Document doc = parser.build(results, "http://www.example.org/");
-		Assert.assertNotNull(doc);
-	}
-
-	@Test
-	public void testValidness_Wrapper() throws Exception {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		RolesOutputStream chemOut = new RolesOutputStream(out);
 		chemOut.close();
@@ -52,17 +45,21 @@ public class RolesOutputStreamTest {
 	}
 
 	@Test
-	public void testValidness_Crash() throws Exception {
+	public void testWellformedness_NamedEntitiesWithRoles() throws Exception {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		RolesOutputStream chemOut = new RolesOutputStream(out);
 		RecoveredChemistry chemistry = new RecoveredChemistry("1");
-		chemistry.addCrash(
-			"This sentence caused a crash.",
-			new Exception("Something went wrong.")
+		List<NamedEntityWithRoles> roles = new ArrayList<NamedEntityWithRoles>();
+		NamedEntityWithRoles entity = new NamedEntityWithRoles("benzene");
+		entity.addRole(new Role("Solvent", "Benzene was used as solvent."));
+		roles.add(entity);
+		chemistry.addRoles(
+			roles
 		);
 		chemOut.write(chemistry);
 		chemOut.close();
 		String results = new String(out.toByteArray());
+		Assert.assertTrue(results.contains("benzene"));
 		Builder parser = new Builder();
 		Document doc = parser.build(results, "http://www.example.org/");
 		Assert.assertNotNull(doc);
